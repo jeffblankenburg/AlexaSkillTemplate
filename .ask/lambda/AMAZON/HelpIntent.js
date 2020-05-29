dfc8a87a7db9a812cba9bb797931fe75e59c07a7
Jeff Blankenburg <github@jeffblankenburg.com>
2020-05-29 11:48:48 -0400

    Initial commit

---
+++ .ask/lambda/AMAZON/HelpIntent.js
@@ -0,0 +1,10 @@
+function HelpIntent(handlerInput) {
+    const speakOutput = 'You can say hello to me! How can I help?';
+
+    return handlerInput.responseBuilder
+        .speak(speakOutput)
+        .reprompt(speakOutput)
+        .getResponse();
+}
+
+module.exports = HelpIntent;
