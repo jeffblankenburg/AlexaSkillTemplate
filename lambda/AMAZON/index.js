5c701daa45e86127867b430096cb13a53aae42de
Fabio William <FabioWilliam@users.noreply.github.com>
2021-04-27 01:16:01 -0300

    Update index.js
    Add a RepeatIntent in index.js

--- lambda/AMAZON/index.js
+++ lambda/AMAZON/index.js
@@ -1,7 +1,9 @@
 const HelpIntent = require("./HelpIntent");
 const StopIntent = require("./StopIntent");
+const RepeatIntent = require("./RepeatIntent");
 
 module.exports = {
     HelpIntent,
     StopIntent,
+    RepeatIntent,
 };
